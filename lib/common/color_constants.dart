c09289bb6c267c9ef4661aa47f34926cf6ccc60d
Vandita Chapadia <vanditachapadia296@gmail.com>
2021-04-19 23:59:37 +0530

    first commit

---
+++ lib/common/color_constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const kwhiteColor = Colors.white;
+  static const kblackColor = Colors.black;
+  static const kgreenColor = Colors.green;
+  static const kgreyColor = Color(0xff909090);
+  static const gblackColor = Color(0xff242424);
+  static const cblackColor = Color(0xFF313131);
+  static const cgreenColor = Colors.green;
+}
